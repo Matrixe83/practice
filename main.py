b7a66c77fe32343c41ee8c40af653b2627d70c2c
Matrixe83 <Matrix.e83@gmail.com>
2017-07-10 18:03:45 +0000

    Change

---
+++ main.py
@@ -0,0 +1,10 @@
+#def maximum(lst):
+#    return max(lst) lst = [1,2,3,15,9,67] print maximum(lst)
+
+
+
+def count(lst):
+  return len(lst)
+  
+lst = ["Abba","Ace of Base","Modern Talking","John","Mike"]
+print count(lst)
